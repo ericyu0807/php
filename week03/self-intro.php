10f0a869cfe778d6a7912c4c1d6f5416ecb3b89f
ericyu0807 <126052061+ericyu0807@users.noreply.github.com>
2024-06-13 17:52:19 +0800

    Create self-intro.php

---
+++ week03/self-intro.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<meta charset"utf-8">
+<meta name="孫晛茗">
+<title>自我介紹
+</title>
+</head>
+    <body bgcolor="#E6CAFF">
+    <font color="black">
+    <center><h2>自我介紹</h2></center>
+    <hr width="100%"color="gray">
+    <a name="top">
+    <center>
+    <a href="self-intro.jpg">
+    <img src="self-intro.jpg"wdith="200"height="200"border="1">
+    </a>
+    </center>
+    <h3>介紹:</h3><br/>
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp我叫<b>余郡恩</b>，出生於台北市，現在就讀國立高雄大學資訊管理學系。我的父親是一位商人，我的母親則是公司會計，有一個弟第，準備升上國中
+    <br/>
+    <h3>基本資料:</h3><br/>
+    <center>
+    <table border="1"bgcolor="white">
+        <tr><td>姓名</td><td>余郡恩</td></tr>
+        <tr><td>暱稱</td><td>地精(小學140、國中160 = =)</td></tr>
+        <tr><td>個性</td><td>內向、活潑、屁話很多</td></tr>
+        <tr><td>愛好</td><td>籃球、保齡球、桌遊(賭博)</td></tr>
+        <tr><td>電話</td><td>0926512588</td></tr>
+        <tr><td>血型</td><td>O</td></tr>
+        <tr><td>星座</td><td>獅子座</td></tr>
+        <tr><td>生日</td><td>2004/08/07</td></tr>
+        <tr><td>目標</td><td>準時畢業</tr>
+    </table>
+
+    沒東西了，<a href="#top">回開頭</a><br/>
+    </center>
+
+</body>
+</html>
